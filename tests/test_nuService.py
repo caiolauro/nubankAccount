59f616660c2891b16285585a213545fc9761f7a3
Caio Lauro <clauro@galileo-ft.com>
2022-04-17 20:24:51 -0300

    remove tests dir

--- tests/test_nuService.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-class TestnuService(unittest.TestCase):
-    def test_current_savings(self, current_savings=123):
-        t=type(current_savings)
-        self.assertEqual(t,int)
