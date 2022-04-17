a4867dc292da34ca8c9e078cb21520c9e9c79d07
Caio Lauro <clauro@galileo-ft.com>
2022-04-17 19:45:05 -0300

    recreate main.py using nuService

---
+++ main.py
@@ -0,0 +1,8 @@
+from nuService import nuService
+
+if __name__ == '__main__':
+    nuService.inititalize()
+    nuService.get_credit_card_spend()
+    nuService.get_debit_account_spend
+    nuService.get_current_savings
+    nuService.update_gsheet()
